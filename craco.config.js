1435a5b203db7d2163e55d2e5897111eedca29e8
James Davies <james.davies@kineticdata.com>
2023-07-18 22:30:52 -0500

    Fixing bug with font awesome icon

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,10 @@
+const AddCharsetWebpackPlugin = require('add-charset-webpack-plugin');
+
 module.exports = {
   webpack: {
+    plugins: {
+      add: [new AddCharsetWebpackPlugin({ charset: 'utf-8' })],
+    },
     configure: webpackConfig => {
       // Fix for strict EcmaScript Module error, not fixed in the latest React Scripts (5.0.1 at the time of this
       // comment) without ejecting and making this change. FYI: This is the only reason for Craco to be used.
